53d70018704f822dcc7d30eae06ff245ed338cd5
crismiche1 <crisnat1905@gmailcom>
2024-09-11 16:59:27 -0300

    Configuramos la base de datos

--- src/database/db.js
+++ src/database/db.js
@@ -5,6 +5,8 @@ const dbConection = async ()=> {
     try {
 
         await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
           
         })
 
